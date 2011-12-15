d5497aee801e518e05477662a44860aa5ebbcd9d
Luis Alejandro Martínez Faneyth <martinez.faneyth@gmail.com>
2011-12-15 00:58:04 -0430

    * Correcting setting of localization. * Minos style adjustments.

--- Locale.php
+++ Locale.php
@@ -1,8 +1,10 @@
 <?php
-echo $app_locale;
-putenv("LC_ALL=$app_locale");
-setlocale(LC_ALL, $app_locale);
-bindtextdomain("messages", "./locale");
+
+putenv("LC_ALL=$app_locale.UTF-8");
+putenv("LANG=$app_locale.UTF-8");
+putenv("LANGUAGE=$app_locale");
+setlocale(LC_ALL, "$app_locale.UTF-8");
+bindtextdomain("messages", "locale");
 textdomain("messages");
 
 ?>
