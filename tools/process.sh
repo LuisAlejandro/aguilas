dfcfba70fe804484de4de3b725fe45ffcfe1bbab
Luis Alejandro Martínez Faneyth <martinez.faneyth@gmail.com>
2012-01-15 09:09:21 -0430

    Improving susbstitution system for generating documentation

--- tools/process.sh
+++ tools/process.sh
@@ -6,18 +6,17 @@ INFILES=$( grep -R "@@.*@@" ${DOCDIR} | awk -F: '{print $1}' | sort -u )
 SUBS="AUTHOR DATE VERSION URL DESCRIPTION DESCRIPTIONLINES COMMONINTRO RSTLIST LINKLIST"
 AUTHOR="Luis Alejandro Martínez Faneyth"
 DATE=$( date +%d-%m-%Y )
-VERSION=$( cat ../VERSION | grep "VERSION = " | sed 's/VERSION = //g' )
+VERSION=$( cat ../VERSION | grep "VERSION = " | sed 's/VERSION = //g;s/+.*//g' )
 URL="http://code.google.com/p/aguilas"
 DESCRIPTION="A web-based LDAP user management system"
 DESCRIPTIONLINES="---------------------------------------"
-COMMONINTRO=$( cat ${DOCDIR}common.index.in )
+COMMONINTRO=$( cat ${DOCDIR}common.index.in | sed ':a;N;$!ba;s/\n/_______/g' )
 RSTLIST=$( ls ${RESTDIR}*.rest | sed "s|${RESTDIR}||g" )
 LINKLIST=$( ls ${RESTDIR}*.rest | sed "s|${RESTDIR}|[[|g;s|.rest|]]|g" )
 
 for INFILE in ${INFILES}; do
 	for SUB in ${SUBS}; do
-		sed -i "s/@@${SUB}@@/$( eval "echo \"\$${SUB}\"" )/g" ${INFILE}
+		sed -i "s|@@${SUB}@@|$( eval "echo \"\$${SUB}\"" )|g" ${INFILE}
+		sed -i "s|_______|\n|g" ${INFILE}
 	done
 done
-
-
