6710c342b2a7d3664d51874d5f95a9dfc4bf1ff1
Luis Alejandro Martínez Faneyth <martinez.faneyth@gmail.com>
2012-02-04 00:46:02 -0430

    Improving release script

--- tools/release.sh
+++ tools/release.sh
@@ -26,14 +26,44 @@
 # CODE IS POETRY
 
 ROOTDIR="$( pwd )"
+PROJDIR="$( dirname ${ROOTDIR} )"
 GITHUBWIKI="${ROOTDIR}/documentation/githubwiki"
-GOOGLBWIKI="${ROOTDIR}/documentation/googlewiki"
+GOOGLEWIKI="${ROOTDIR}/documentation/googlewiki"
 VERSION="${ROOTDIR}/VERSION"
 CHANGELOG="${ROOTDIR}/ChangeLog"
 CHANGES="$( tempfile )"
 DEVERSION="$( tempfile )"
 NEWCHANGES="$( tempfile )"
 DATE=$( date +%D )
+
+if [ "$( git branch 2> /dev/null | sed -e '/^[^*]/d;s/\* //' )" != "development" ]; then
+	echo "[MAIN] You are not on \"development\" branch."
+	git checkout development
+fi
+if [ "$( git diff --exit-code 2> /dev/null )" != "0" ]; then
+	echo "[MAIN] You have uncommitted code on \"development\" branch."
+	exit 1
+fi
+cd ${GITHUBWIKI}
+if [ "$( git branch 2> /dev/null | sed -e '/^[^*]/d;s/\* //' )" != "development" ]; then
+	echo "[GITHUBWIKI] You are not on \"development\" branch."
+	git checkout development
+fi
+if [ "$( git diff --exit-code 2> /dev/null )" != "0" ]; then
+	echo "[GITHUBWIKI] You have uncommitted code on \"development\" branch."
+	exit 1
+fi
+cd ${ROOTDIR}
+cd ${GOOGLEWIKI}
+if [ "$( git branch 2> /dev/null | sed -e '/^[^*]/d;s/\* //' )" != "development" ]; then
+	echo "[GOOGLEWIKI] You are not on \"development\" branch."
+	git checkout development
+fi
+if [ "$( git diff --exit-code 2> /dev/null )" != "0" ]; then
+	echo "[GOOGLEWIKI] You have uncommitted code on \"development\" branch."
+	exit 1
+fi
+cd ${ROOTDIR}
 
 git log > ${CHANGES}
 cp ${VERSION} ${DEVERSION}
@@ -50,49 +80,53 @@ sed -i 's/New development snapshot.*//g' ${NEWCHANGES}
 echo "" >> ${NEWCHANGES}
 cat ${CHANGELOG} >> ${NEWCHANGES}
 
-git merge -s recursive -X theirs development
+git merge -s recursive -X theirs --squash development
 
 mv ${NEWCHANGES} ${CHANGELOG}
 rm ${CHANGES} ${DEVERSION}
 
-LASTCOMMIT=$( git rev-parse HEAD )
+LASTCOMMIT="$( git rev-parse HEAD )"
 
 echo "VERSION = ${NEWVERSION}" > ${VERSION}
 echo "COMMIT = ${LASTCOMMIT}" >> ${VERSION}
 
+make prepare
+
 cd ${GOOGLEWIKI}
 git checkout master
-git merge -s recursive -X theirs development
-git push --tags https://code.google.com/p/aguilas.wiki/ master
+git merge -s recursive -X theirs --squash development
+git add .
+git commit -a -m "Updating documentation"
+#git push --tags https://code.google.com/p/aguilas.wiki/ master
 git checkout development
 cd ${ROOTDIR}
 
 cd ${GITHUBWIKI}
 git checkout master
-git merge -s recursive -X theirs development
-git push --tags git@github.com:HuntingBears/aguilas.wiki.git master
+git merge -s recursive -X theirs --squash development
+git add .
+git commit -a -m "Updating documentation"
+#git push --tags git@github.com:HuntingBears/aguilas.wiki.git master
 git checkout development
 cd ${ROOTDIR}
 
-git checkout release
-
 git add .
 git commit -a -m "New stable release ${NEWVERSION}"
-git tag ${NEWVERSION} -m "New stable release ${NEWVERSION}"
+#git tag ${NEWVERSION} -m "New stable release ${NEWVERSION}"
 
-git push --tags git@github.com:HuntingBears/aguilas.git release
-git push --tags git@gitorious.org:huntingbears/aguilas.git release
-git push --tags https://code.google.com/p/aguilas/ release
+#git push --tags git@github.com:HuntingBears/aguilas.git release
+#git push --tags git@gitorious.org:huntingbears/aguilas.git release
+#git push --tags https://code.google.com/p/aguilas/ release
 
-git archive -o aguilas_${NEWVERSION}.orig.tar.gz ${NEWVERSION}
-md5sum aguilas_${NEWVERSION}.orig.tar.gz > aguilas_${NEWVERSION}.orig.tar.gz.md5
+#git archive -o aguilas_${NEWVERSION}.orig.tar.gz ${NEWVERSION}
+#md5sum aguilas_${NEWVERSION}.orig.tar.gz > aguilas_${NEWVERSION}.orig.tar.gz.md5
 
-python -B googlecode-upload.py -s "AGUILAS RELEASE ${NEWVERSION}" \
-	-p "aguilas" -l "Type-Archive,Type-Source,OpSys-Linux,Featured,Stable" \
-	aguilas_${NEWVERSION}.orig.tar.gz
-python -B googlecode-upload.py -s "AGUILAS RELEASE ${NEWVERSION} MD5SUM" \
-	-p "aguilas" -l "Featured,Stable" aguilas_${NEWVERSION}.orig.tar.gz.md5
+#python -B googlecode-upload.py -s "AGUILAS RELEASE ${NEWVERSION}" \
+#	-p "aguilas" -l "Type-Archive,Type-Source,OpSys-Linux,Featured,Stable" \
+#	aguilas_${NEWVERSION}.orig.tar.gz
+#python -B googlecode-upload.py -s "AGUILAS RELEASE ${NEWVERSION} MD5SUM" \
+#	-p "aguilas" -l "Featured,Stable" aguilas_${NEWVERSION}.orig.tar.gz.md5
 
-mv aguilas*.tar.gz* ..
+#mv aguilas*.tar.gz* ..
 
 git checkout development
